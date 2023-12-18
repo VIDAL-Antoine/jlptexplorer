fff0fe1b8482eefa91a0093d11ab25fd21d5c95f
VIDAL-Antoine <avidal78390@gmail.com>
2023-12-18 20:28:29 +0100

    Ajout liaison back-front

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("game_gramexplorer");
+    const collection = database.collection("grammar_points_n5");
+    const documents = await collection.find({}).toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import DataDisplay from './DataDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <DataDisplay />
+    </div>
+  );
+}
+
+export default App;
