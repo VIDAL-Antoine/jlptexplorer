1b449bf1b1f54766000fa55f72f66bca8361c2e0
VIDAL-Antoine <avidal78390@gmail.com>
2023-12-19 18:14:13 +0100

    Fix navbar collé au bord

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DataDisplay from './components/DataDisplay';
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const linkStyle = {
-    marginRight: '2px', // Adjust the value according to your needs
-};
-
 const Header = ({ setJlptLevel }) => {
   return (
     <header className="bg-dark text-white py-3">
       <nav className="navbar navbar-dark">
-        <Link to="/" style={linkStyle} className="nav-link ml-2 mr-3">Home</Link>
+        <Link to="/" style={{ marginLeft: '20px' }} className="nav-link px-20">Home</Link>
         <Link to="/n5" className="nav-link" onClick={() => setJlptLevel('n5')}>N5</Link>
         <Link to="/n4" className="nav-link" onClick={() => setJlptLevel('n4')}>N4</Link>
         <Link to="/n3" className="nav-link" onClick={() => setJlptLevel('n3')}>N3</Link>
@@ -18,7 +14,7 @@ const Header = ({ setJlptLevel }) => {
         <Link to="/n1" className="nav-link" onClick={() => setJlptLevel('n1')}>N1</Link>
         <Link to="/other" className="nav-link">Other</Link>
         <Link to="/faq" className="nav-link">FAQ</Link>
-        <Link to="/contact" className="nav-link ml-2">Contact</Link>
+        <Link to="/contact" style={{ marginRight: '20px' }} className="nav-link ml-2">Contact</Link>
       </nav>
     </header>
   );
