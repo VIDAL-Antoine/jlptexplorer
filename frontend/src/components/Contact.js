bb517e75f06153cfe6d96f0bf800159b56fd0b48
VIDAL-Antoine <avidal78390@gmail.com>
2023-12-21 16:55:26 +0100

    Retrait setJlptLevel dans Contact

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Contact = ({ setJlptLevel }) => {
+const Contact = () => {
   return (
     <div className="container text-center mt-5">
         <h1 className="display-4">Contact</h1>
