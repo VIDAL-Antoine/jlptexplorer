f9f0137e9890a06cdd871c38e45a02f499abdcba
VIDAL-Antoine <avidal78390@gmail.com>
2023-12-28 19:17:41 +0100

    Responsive videos + home icon

--- frontend/src/components/DetailedView.js
+++ frontend/src/components/DetailedView.js
@@ -33,58 +33,58 @@ const DetailedView = () => {
 
   return (
     <div>
-    {(!grammarPoint) ?
-    (      
-      <div className="d-flex justify-content-center align-items-center">
-        <div className="spinner-border my-5" role="status">
+      {(!grammarPoint) ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="spinner-border my-5" role="status"></div>
         </div>
-      </div>
-    ) : (
-      <div className="container mt-4">
-        <DetailedViewWrapper>
-        <div className="d-flex align-items-center py-3 ms-2">
-          <i className="bi bi-book-fill" style={{ fontSize: '2rem', color: '#006400'}} />
-          <h1 className="ms-3">{grammarPoint.title}</h1>
-        </div>
-        </DetailedViewWrapper>
-
-        <div className='d-flex'>
-          <DetailedViewWrapper className='d-flex align-items-center' style={{ flex: 1, marginRight: '30px' }}>
-            <i className="bi bi-translate ms-2" style={{ fontSize: '2rem', color: '#007BFF'}} />
-            <span className='ms-3' style={{ fontSize: '18px' }}>{grammarPoint.englishTranslation}</span>
-          </DetailedViewWrapper>
-          <DetailedViewWrapper className='d-flex align-items-center' style={{ textAlign: 'center' }}>
-            <i className="bi bi-graph-up-arrow" style={{ fontSize: '2rem', color: '#800080'}} />
-            <span className='ms-3' style={{ fontSize: '18px' }}>{grammarPoint.jlptLevel}</span>
-          </DetailedViewWrapper>
-        </div>
-
-        <DetailedViewWrapper>
-          <h3 className='mb-3 ms-2'>
-            <i className="bi bi-chat-dots" style={{ fontSize: '2rem', color: '#cc6600'}} />
-            <span className='ms-3'>Examples</span>
-          </h3>
-          {grammarPoint.examples.map((example, index) => (
-            <div key={index} className="card mb-4">
-              <div className="card-body">
-                <p className="card-text">Sentence: {example.sentence}</p>
-                <p className="card-text">Source: {example.source}</p>
-                <p className="card-text text-center">
-                  <iframe
-                    width="560"
-                    height="315"
-                    src={example.videoURL} // https://www.youtube.com/embed/eI4an8aSsgw works
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </p>
-              </div>
+      ) : (
+        <div className="container mt-4">
+          <DetailedViewWrapper>
+            <div className="d-flex align-items-center py-3 ms-2">
+              <i className="bi bi-book-fill" style={{ fontSize: '2rem', color: '#006400' }} />
+              <h1 className="ms-3">{grammarPoint.title}</h1>
             </div>
-          ))}
-        </DetailedViewWrapper>
-      </div>
-    )}
+          </DetailedViewWrapper>
+
+          <div className='d-flex'>
+            <DetailedViewWrapper className='d-flex align-items-center' style={{ flex: 1, marginRight: '30px' }}>
+              <i className="bi bi-translate ms-2" style={{ fontSize: '2rem', color: '#007BFF'}} />
+              <span className='ms-3' style={{ fontSize: '18px' }}>{grammarPoint.englishTranslation}</span>
+            </DetailedViewWrapper>
+            <DetailedViewWrapper className='d-flex align-items-center' style={{ textAlign: 'center' }}>
+              <i className="bi bi-graph-up-arrow ms-2" style={{ fontSize: '2rem', color: '#800080'}} />
+              <span className='ms-3' style={{ fontSize: '18px' }}>{grammarPoint.jlptLevel}</span>
+            </DetailedViewWrapper>
+          </div>
+
+          <DetailedViewWrapper>
+            <h3 className="mb-3 ms-2">
+              <i className="bi bi-chat-dots" style={{ fontSize: '2rem', color: '#cc6600'}} />
+              <span className="ms-3">Examples</span>
+            </h3>
+            {grammarPoint.examples.map((example, index) => (
+              <div key={index} className="card mb-4">
+                <div className="card-body">
+                  <p className="card-text">Sentence: {example.sentence}</p>
+                  <p className="card-text">Source: {example.source}</p>
+                  <div className="embed-responsive embed-responsive-16by9" style={{ maxWidth: '1024px', width: '100%', margin: 'auto' }} >
+                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
+                      <iframe
+                        className="embed-responsive-item"
+                        src={example.videoURL}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        style={{ position: 'absolute', width: '100%', height: '100%' }}
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </DetailedViewWrapper>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
   return (
     <Navbar color="primary" dark expand="md">
        <Nav className="d-flex justify-content-between w-100 mx-5" navbar>
-          <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+          <NavItem><Link to="/" className="nav-link"><i class="bi bi-house-fill"></i></Link></NavItem>
           <NavItem><Link to="/n5" className="nav-link" >N5</Link></NavItem>
           <NavItem><Link to="/n4" className="nav-link" >N4</Link></NavItem>
           <NavItem><Link to="/n3" className="nav-link" >N3</Link></NavItem>
           <NavItem><Link to="/n2" className="nav-link" >N2</Link></NavItem>
           <NavItem><Link to="/n1" className="nav-link" >N1</Link></NavItem>
-          <NavItem><Link to="/nu" className="nav-link" >Unclassified</Link></NavItem>
+          {/* <NavItem><Link to="/nu" className="nav-link" >Unclassified</Link></NavItem> */}
         </Nav>
     </Navbar>
   );
